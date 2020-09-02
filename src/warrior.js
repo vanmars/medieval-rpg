026a3e5a4a0bfb67c1a2432f7f81ca0880fbe407
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-02 10:03:21 -0700

    Add initial file structure with planning in main.js and first class of warrior defined

---
+++ src/warrior.js
@@ -0,0 +1,22 @@
+// Warrior
+      // strength:100
+      // HP (health points): 100
+      // defense: 80
+      // intelligence: 20
+      // Roll Method: for special power, return is how much damage cause
+
+class Warrior {
+  constructor(){
+    this.strength = 100;
+    this.healthPoints = 100;
+    this.defense = 80;
+    this.intelligence = 20;
+  }
+  roll() {
+    return -2(Math.ceil(Math.random()*6))
+  }
+
+  applyDamage(number) {
+    this.healthPoints -= number;
+  }
+}
